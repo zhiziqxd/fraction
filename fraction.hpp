0ce54eb7c333011e03dcf70025987b9202656956
zhiziqxd <166890319+zhiziqxd@users.noreply.github.com>
2024-10-04 19:03:08 +0800

    Update fraction.hpp

--- fraction.hpp
+++ fraction.hpp
@@ -5,8 +5,8 @@
 #include <limits.h>
 using namespace std;
 struct fraction{
-	int de,mol;
-    void sim(){
+	int de,mol;//分母，分子
+    void sim(){//约分
         for(int i=min(de,mol);i>=2;i--){
             while(de%i==0&&mol%i==0){
                 de/=i;
@@ -15,22 +15,41 @@ struct fraction{
         }
         return;
     }
-    bool check(){
+    bool check(){//检查是否合法
         if(de!=0&&de!=-1&&mol!=-1){
             return true;
         }
         return false;
     }
-    void clear(){
+    void clear(){//清空
         de=0;
         mol=0;
         return;
     }
 };
-fraction fra_count(fraction a,fraction b,short form){
+//比较大小
+bool fra_big(fraction a,fraction b){
+	if(a.mol*b.de>b.mol*a.de){
+		return true;
+	}
+	return false;
+}
+bool fra_small(fraction a,fraction b){
+	if(a.mol*b.de<b.mol*a.de){
+		return true;
+	}
+	return false;
+}
+bool fra_amount(fraction a,fraction b){
+	if(a.mol*b.de==b.mol*a.de){
+		return true;
+	}
+	return false;
+}
+fraction fra_count(fraction a,fraction b,short form){//计算
 	fraction ans={1,0};
 	switch(form){
-		case 1:
+		case 1://加法
 		if(a.de==b.de){
 			ans.de=a.de;
 			ans.mol=a.mol+b.mol;
@@ -40,20 +59,20 @@ fraction fra_count(fraction a,fraction b,short form){
 			ans.mol=a.mol*b.de+b.mol*a.de;
 		}
         break;
-		case 2:
+		case 2://减法
 		if(a.de==b.de){
 			if(a.mol>=b.mol){
 				ans.de=a.de;
 				ans.mol=a.mol-b.mol;
 			}
-			else{
+			else{//出现错误
                 ans.de=-1;
                 ans.mol=-1;
 				cout<<"error:a is bigger than b";
 			}
 		}
 		else{
-			if(a.mol*b.de-b.mol*a.de<0){
+			if(a.mol*b.de-b.mol*a.de<0){//出现错误
 				cout<<"error:a is bigger than b";
                 ans.de=-1;
                 ans.mol=-1;
@@ -64,15 +83,15 @@ fraction fra_count(fraction a,fraction b,short form){
 			}
 		}
         break;
-		case 3:
+		case 3://乘法
 		ans.de=a.de*b.de;
         ans.mol=a.mol*b.mol;
         break;
-		case 4:
+		case 4://除法
         ans.de=a.de*b.mol;
         ans.mol=a.mol*b.de;
         break;
 	}
-    return ans;
+    return ans;//返回
 }
 #endif
